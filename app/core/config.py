f9760f3d9b9944a46c8c271821ff83d29bb01f52
Carlos R <carlos67.9057@gmail.com>
2026-01-28 14:46:05 -0600

    Configure url

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+# app/core/config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    app_name: str = "Payment Events Processor"
+    database_url: str = "postgresql+psycopg://postgres:postgres@localhost:5432/payment_events"
+
+settings = Settings()
